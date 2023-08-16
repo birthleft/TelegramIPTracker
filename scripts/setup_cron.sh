50c2ffaea01ce92b3b34aece731fe5f18780f3e4
TAVI <octavianpatilea@gmail.com>
2023-08-16 22:58:24 +0300

    Updated cron setup to include npm install

--- scripts/setup_cron.sh
+++ scripts/setup_cron.sh
@@ -3,6 +3,8 @@
 # Change directory to the location of your Node.js program
 cd ../src/
 
+npm install
+
 # Set up the cron job to run the Node.js program every 30 minutes
 (crontab -l 2>/dev/null; echo "*/30 * * * * /usr/bin/node -r dotenv/config index.js") | crontab -
 echo "Cron job set up successfully."
