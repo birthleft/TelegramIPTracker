10aea208a4afc099f55da5ee13db07f839ff6eb6
TAVI <octavianpatilea@gmail.com>
2023-08-16 22:50:07 +0300

    Added dotenv functionality to scripts

--- scripts/setup_cron.sh
+++ scripts/setup_cron.sh
@@ -4,5 +4,5 @@
 cd ../src/
 
 # Set up the cron job to run the Node.js program every 30 minutes
-(crontab -l 2>/dev/null; echo "*/30 * * * * /usr/bin/node index.js") | crontab -
+(crontab -l 2>/dev/null; echo "*/30 * * * * /usr/bin/node -r dotenv/config index.js") | crontab -
 echo "Cron job set up successfully."
